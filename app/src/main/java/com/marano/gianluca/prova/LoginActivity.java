95273ff3891a611f5adf2b79e8b9b4bb77c7766f
Gianluca Marano <gianluca.marano1@gmail.com>
2017-02-10 14:53:25 +0100

    Test Commit Android Studio

--- app/src/main/java/com/marano/gianluca/prova/LoginActivity.java
+++ app/src/main/java/com/marano/gianluca/prova/LoginActivity.java
@@ -1,14 +1,31 @@
 package com.marano.gianluca.prova;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements View.OnClickListener {
+
+    LinearLayout loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginBtn = (LinearLayout) findViewById(R.id.login_btn);
+        loginBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.login_btn:
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                finish();
+                break;
+        }
     }
 }
--- app/src/main/java/com/marano/gianluca/prova/MainActivity.java
+++ app/src/main/java/com/marano/gianluca/prova/MainActivity.java
@@ -1,20 +1,34 @@
 package com.marano.gianluca.prova;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by gianl on 08/02/2017.
  */
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    Button leggi1Btn, leggi2Btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent (this, LoginActivity.class);
-        startActivity(intent);
+        leggi1Btn = (Button) findViewById(R.id.main_leggi1_btn);
+        leggi1Btn.setOnClickListener(this);
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.main_leggi1_btn:
+                Snackbar.make(findViewById(android.R.id.content), "Scemo chi legge", Snackbar.LENGTH_LONG).show();
+                break;
+        }
     }
 }
